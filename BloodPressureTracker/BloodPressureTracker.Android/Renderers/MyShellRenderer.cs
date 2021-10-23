11f23481cc78c4b882e5e978ab5010fb6223ddeb
Luce Carter <luce@lucyscarter.co.uk>
2021-10-23 22:52:15 +0100

    Added images to platform projects

---
+++ BloodPressureTracker/BloodPressureTracker.Android/Renderers/MyShellRenderer.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Android.Content;
+using BloodPressureTracker;
+using BloodPressureTracker.Droid.Renderers;
+using Google.Android.Material.BottomNavigation;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(AppShell), typeof(MyShellRenderer))]
+namespace BloodPressureTracker.Droid.Renderers
+{
+    public class MyShellRenderer : ShellRenderer
+    {
+        public MyShellRenderer(Context context) : base(context)
+        {
+        }
+
+        protected override IShellBottomNavViewAppearanceTracker CreateBottomNavViewAppearanceTracker(ShellItem shellItem)
+        {
+            return new CustomBottomNavAppearanceTracker(this);
+        }
+
+        internal class CustomBottomNavAppearanceTracker : IShellBottomNavViewAppearanceTracker
+        {
+
+            private MyShellRenderer myShellRenderer = null;
+
+            public CustomBottomNavAppearanceTracker(MyShellRenderer myShellRenderer)
+            {
+                this.myShellRenderer = myShellRenderer;
+            }
+
+            public void Dispose()
+            {
+                //throw new System.NotImplementedException();
+            }
+
+            public void ResetAppearance(BottomNavigationView bottomView)
+            {
+                bottomView.ItemIconTintList = null;
+            }
+
+            public void SetAppearance(BottomNavigationView bottomView, ShellAppearance appearance)
+            {
+                bottomView.ItemIconTintList = null;
+            }
+
+            public void SetAppearance(BottomNavigationView bottomView, IShellAppearanceElement appearance)
+            {
+                bottomView.ItemIconTintList = null;                
+            }
+        }
+    }
+}
---
+++ BloodPressureTracker/BloodPressureTracker.iOS/Renderers/MyShellRenderer.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using BloodPressureTracker;
+using BloodPressureTracker.iOS.Renderers;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(AppShell), typeof(MyShellRenderer))]
+namespace BloodPressureTracker.iOS.Renderers
+{
+    public class MyShellRenderer : ShellRenderer
+    {
+        protected override IShellSectionRenderer CreateShellSectionRenderer(ShellSection shellSection) { var renderer = base.CreateShellSectionRenderer(shellSection); if (renderer != null) { } return renderer; }
+
+        protected override IShellTabBarAppearanceTracker CreateTabBarAppearanceTracker()
+        {
+            return new TabbarIconsAppearance();
+        }
+    }
+
+    public class TabbarIconsAppearance : IShellTabBarAppearanceTracker
+    {
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        public void ResetAppearance(UITabBarController controller)
+        {
+            DisplayColorIcons(controller);
+        }
+
+        public void SetAppearance(UITabBarController controller, ShellAppearance appearance)
+        {
+            DisplayColorIcons(controller);
+        }
+
+        public void UpdateLayout(UITabBarController controller)
+        {
+            DisplayColorIcons(controller);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+        }
+
+        private void DisplayColorIcons(UITabBarController controller)
+        {
+            if (controller.TabBar.Items != null)
+            {
+                foreach (UITabBarItem tabbaritem in controller.TabBar.Items)
+                {
+                    tabbaritem.Image = tabbaritem.Image.ImageWithRenderingMode(UIImageRenderingMode.AlwaysOriginal);
+                    tabbaritem.SelectedImage = tabbaritem.SelectedImage.ImageWithRenderingMode(UIImageRenderingMode.AlwaysOriginal);
+                }
+            }
+        }
+    }
+}
+
