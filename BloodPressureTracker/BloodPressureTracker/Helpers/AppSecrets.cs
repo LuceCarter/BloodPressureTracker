7911015c52acd2b0e723424f26d86bfd80ea66e6
Luce Carter <luce@lucyscarter.co.uk>
2021-10-24 11:22:49 +0100

    Added app secrets with templated value

---
+++ BloodPressureTracker/BloodPressureTracker/Helpers/AppSecrets.cs
@@ -0,0 +1,8 @@
+﻿using System;
+namespace BloodPressureTracker.Helpers
+{
+    public static class AppSecrets
+    {
+        public const string RealmAppId = "<ENTER YOUR REALM ID HERE>";
+    }
+}
